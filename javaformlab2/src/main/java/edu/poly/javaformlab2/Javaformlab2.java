6cc231f337aa1b8ba8fd85a793a7e1ff7bd1f385
trieudatly <lytrieudat@gmail.com>
2022-11-18 21:57:44 +0700

    learn javaform

---
+++ javaformlab2/src/main/java/edu/poly/javaformlab2/Javaformlab2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package edu.poly.javaformlab2;
+
+/**
+ *
+ * @author lytri
+ */
+public class Javaformlab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
