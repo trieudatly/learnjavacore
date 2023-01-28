e63b2b78e0c1e21dddbce7f82c2dae012e7bd694
trieudatly <lytrieudat@gmail.com>
2023-01-28 21:28:27 +0700

    add ColorSlider

---
+++ DemoSlider/src/main/java/edu/poly/demoslider/DemoSlider.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.poly.demoslider;
+
+/**
+ *
+ * @author lytri
+ */
+public class DemoSlider {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
